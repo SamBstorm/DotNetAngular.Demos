228d4b3dc8292d5061082d1ae6687bf27971db09
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-05 10:35:26 +0200

    Ajoutez des fichiers projet.

---
+++ DotNetAngular.Demos.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetAngular.Demos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public static WeatherForecast wf { get; set; }
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+        [HttpGet]
+        [Route("List")]
+        public IEnumerable<WeatherForecast> List()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("{id}")]
+        public WeatherForecast Get(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[id];
+        }
+
+        [HttpPost]
+        public ActionResult Post()
+        {
+            if (wf is null)
+            {
+                wf = new WeatherForecast() { TemperatureC = 12, Date = DateTime.Now, Summary = "Cloudy" };
+                return Ok();
+            }
+            return BadRequest(new { status = 405 });
+        }
+
+        /*
+        GET(All)  : domain/[controller]/ 
+        GET  : domain/[controller]/{id}
+        POST : domain/[controller]/
+        PUT  : domain/[controller]/{id}
+        DELETE : domain/[controller]/{id}
+         */
+    }
+}
