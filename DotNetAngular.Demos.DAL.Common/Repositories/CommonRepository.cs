f1da14b3ee466676513afda68710cbee69c9621e
Legrain Samuel <samuel.legrain@bstorm.be>
2021-05-11 10:20:28 +0200

    Unfinished DAL Global ConnectionString Integration

---
+++ DotNetAngular.Demos.DAL.Common/Repositories/CommonRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DotNetAngular.Demos.DAL.Common.Repositories
+{
+    public abstract class CommonRepository
+    {
+        protected IConfiguration _configuration;
+        private string _dbName;
+        protected string _connectionString => _configuration.GetConnectionString(_dbName);
+        protected DbProviderFactory _factory => DbProviderFactories.GetFactory("System.Data.SqlClient");
+
+        //private string _connectionString => _configuration.GetValue<string>($"Modules:{_dbName}:ConnectionString");
+        //private DbProviderFactory _factory => DbProviderFactories.GetFactory(_configuration.GetValue<string>($"Modules:{_dbName}:InvariantName"));
+        public CommonRepository(IConfiguration config, string dbName)
+        {
+            _configuration = config;
+            _dbName = dbName;
+        }
+    }
+}
---
+++ DotNetAngular.Demos.DAL.Global/Repositories/MoviesRepository.cs
@@ -0,0 +1,52 @@
+﻿using DotNetAngular.Demos.DAL.Common.Repositories;
+using DotNetAngular.Demos.DAL.Common.Services;
+using DotNetAngular.Demos.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+//using Tools.Connections.Database;
+
+namespace DotNetAngular.Demos.DAL.Global.Repositories
+{
+    public class MoviesRepository : CommonRepository, IMovieService<Movie, int>
+    {
+        public MoviesRepository(IConfiguration config) : base(config,"MovieDB")
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie Get(int id)
+        {
+            //Connection connection = new Connection(_factory,_connectionString);
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetByYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Movie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Movie entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
